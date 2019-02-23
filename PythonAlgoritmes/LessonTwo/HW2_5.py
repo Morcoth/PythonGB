928ea209df8240396f99fa6c80933d3f5eb026b1
Frilled Lizard <frilledlizard@localhost.localdomain>
2019-02-23 14:41:30 +0300

    HW2_5

--- PythonAlgoritmes/LessonTwo/HW2_5.py
+++ PythonAlgoritmes/LessonTwo/HW2_5.py
@@ -1,3 +1,13 @@
 #Вывести на экран коды и символы таблицы ASCII, начиная с символа под номером 32 и заканчивая 127-м включительно. 
 # Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 
+STARTCODE = 32
+ENDCODE = 128
+
+for i in range (STARTCODE, ENDCODE):    
+    print (STARTCODE, chr(STARTCODE))
+    STARTCODE+=1
+    if i%10==0:
+        print ()
+
+
