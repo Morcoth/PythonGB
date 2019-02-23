956264e45182402cd21c775765a71e0b30cac5b8
Anton Buydalin <33263089+Morcoth@users.noreply.github.com>
2019-02-23 14:53:59 +0300

    HW2_5

--- PythonAlgoritmes/LessonTwo/HW2_5.py
+++ PythonAlgoritmes/LessonTwo/HW2_5.py
@@ -5,9 +5,7 @@ STARTCODE = 32
 ENDCODE = 128
 
 for i in range (STARTCODE, ENDCODE):    
-    print (STARTCODE, chr(STARTCODE))
+    print (STARTCODE, chr(STARTCODE), end = " ")
     STARTCODE+=1
     if i%10==0:
         print ()
-
-
