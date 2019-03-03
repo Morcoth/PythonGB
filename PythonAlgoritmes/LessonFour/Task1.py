1b58dee336c3c8a13fabecee984355fbc0aa526e
Frilled Lizard <frilledlizard@localhost.localdomain>
2019-03-03 17:43:31 +0300

    fin

--- PythonAlgoritmes/LessonFour/Task1.py
+++ PythonAlgoritmes/LessonFour/Task1.py
@@ -3,14 +3,14 @@
 
 # Задание 1 из 3его урока. В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны любому из чисел в диапазоне от 2 до 9.
 
-# import timeit
-# import cProfile
+import timeit
+import cProfile
 
 
 def mat (i):
     count = 0
     while i<10:
-        for y in range (2,100):
+        for y in range (2,1000000):
             if y%i==0:
                 count=count+1
         #print (i, count) 
@@ -23,18 +23,20 @@ def main(n):
 #print(timeit.timeit("mat(2)", 'from __main__ import mat', number=100))
 #0.014456499999999983
 # 1.125776 с выводом значений
-#cProfile.run('main(10000000)')
+cProfile.run('main(1000)')
 #C Принтом        
         #100 loops, best of 3: 89.2 usec per loop
         #1000 loops, best of 3: 92.4 usec per loop
         #10000 loops, best of 3: 98.3 usec per loop
         #100000 loops, best of 3: 103 usec per loop
 #Без принта
-        #100000 loops, best of 3: 35.7 usec per loop
+#       9/1    0.821    0.091    0.821    0.821 Task1.py:10(mat)
+#         1    0.000    0.000    0.821    0.821 Task1.py:20(main)
+                 #100000 loops, best of 3: 35.7 usec per loop
 def mat2 ():
     for i in range (2,10):
             count=0
-            for y in range (2,100):
+            for y in range (2,1000000):
                     if y%i==0:
                              count=count+1
             #print (i, count)
@@ -44,8 +46,10 @@ def main2(n):
 #0.01600669999999993 без вывода значений
 # 1.3244124999999998 с выводом значений
 
-#cProfile.run('main2(10000000)')
+cProfile.run('main2(1000)')
 #100 loops, best of 3: 70.6 usec per loop
 #1000 loops, best of 3: 77.9 usec per loop
 #10000 loops, best of 3: 94.8 usec per loop
 #100000 loops, best of 3: 100 usec per loop
+# 1    0.814    0.814    0.814    0.814 Task1.py:34(mat2)
+# 1    0.000    0.000    0.814    0.814 Task1.py:41(main2)
--- PythonAlgoritmes/LessonFour/Task2.py
+++ PythonAlgoritmes/LessonFour/Task2.py
@@ -1,9 +1,28 @@
-def prime_eratosthenes(n):
-    prime_list = []
-    for i in range(2, n+1):
-        if i not in prime_list:
-            print (i)
-            for j in range(i*i, n+1, i):
-                prime_list.append(j)
+import math
+import timeit
+import cProfile
 
-print(prime_eratosthenes(100))
+
+def foo(n):
+    dictind = {}
+    keys_for_dict=1
+    for i in range(2, 10000):
+        flag = True
+        for j in range(2, 1 + int(math.sqrt(i))):
+            if not i % j:
+                flag = False
+                break
+        if flag:
+            dictind.update({keys_for_dict:i})
+            keys_for_dict=keys_for_dict+1
+    print (f"{n}= {dictind.get(n)}")
+
+
+def main(n):
+    foo(50)   
+
+#cProfile.run('main(1000)')
+
+#  1    0.015    0.015    0.018    0.018 Task2.py:6(foo)
+print(timeit.timeit("foo(50)", 'from __main__ import foo', number=100))
+# 1.9100934669995695 для 100 вызовов
