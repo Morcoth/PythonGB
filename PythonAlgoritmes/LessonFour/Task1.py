d787c166da3733775d7c9fa08c5c70ca1c13c498
Frilled Lizard <frilledlizard@localhost.localdomain>
2019-03-03 11:41:05 +0300

    T1

--- PythonAlgoritmes/LessonFour/Task1.py
+++ PythonAlgoritmes/LessonFour/Task1.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
 # Задание 1 из 3его урока. В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны любому из чисел в диапазоне от 2 до 9.
-import timeit
-import cProfile
+
+# import timeit
+# import cProfile
 
 
 def mat (i):
@@ -9,7 +13,7 @@ def mat (i):
         for y in range (2,100):
             if y%i==0:
                 count=count+1
-        print (f"для числа {i} кратных чисел {count}") 
+        #print (i, count) 
         i=i+1            
         mat(i)
         return None 
@@ -20,14 +24,20 @@ def main(n):
 #0.014456499999999983
 # 1.125776 с выводом значений
 #cProfile.run('main(10000000)')
-
+#C Принтом        
+        #100 loops, best of 3: 89.2 usec per loop
+        #1000 loops, best of 3: 92.4 usec per loop
+        #10000 loops, best of 3: 98.3 usec per loop
+        #100000 loops, best of 3: 103 usec per loop
+#Без принта
+        #100000 loops, best of 3: 35.7 usec per loop
 def mat2 ():
     for i in range (2,10):
             count=0
             for y in range (2,100):
                     if y%i==0:
                              count=count+1
-            print (f"для числа {i} кратных чисел {count}")
+            #print (i, count)
 def main2(n):
     mat2()
 #print(timeit.timeit("mat2()", 'from __main__ import mat2', number=100))
@@ -35,3 +45,7 @@ def main2(n):
 # 1.3244124999999998 с выводом значений
 
 #cProfile.run('main2(10000000)')
+#100 loops, best of 3: 70.6 usec per loop
+#1000 loops, best of 3: 77.9 usec per loop
+#10000 loops, best of 3: 94.8 usec per loop
+#100000 loops, best of 3: 100 usec per loop
