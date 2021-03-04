7fb20855d0dd8a849d67e216027248ba61254970
Anton Buydalin <animuscaligo@gmail.com>
2021-03-04 12:28:41 +0300

    комимит

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import psycopg2 as pg
-#import pandas.io.sql as psql
-#import pandas as pd
-import configparser  # импортируем библиотеку
+import configparser 
 import re
 import os
 # Реализовать сценарий загрузки архивных CorpId и SsoId из лог-файлов продуктивного контура
@@ -12,32 +10,32 @@ import os
 
 def updateTable(eno, corpId, ssoId):
     try:
-        connection = pg.connect(dbname='op_dev_morcoth', user='postgres', 
-                        password='123', host='localhost', port='5433')
+        connection = pg.connect(dbname='ELMA3', user='postgres', 
+                        password='1', host='10.176.76.212', port='5432')
 
         cursor = connection.cursor()
 
-        print("Table Before updating record ")
-        sql_select_query = """select * from recource where service_number = %s"""
-        cursor.execute(sql_select_query, (eno,))
-        record = cursor.fetchone()
-        print(record)
+        # print("Table Before updating record ")
+        # sql_select_query = """select * from recourse where eno = %s"""
+        # cursor.execute(sql_select_query, (eno,))
+        # record = cursor.fetchone()
+        # print(record)
 
         # Update single record now
-        sql_update_corpId = """Update recource set CorpId = %s where service_number = %s"""
+        sql_update_corpId = """Update recourse set corpid = %s where eno = %s"""
         cursor.execute(sql_update_corpId, (corpId, eno))
-        sql_update_ssoId = """Update recource set CorpId = %s where service_number = %s"""
+        sql_update_ssoId = """Update recourse set sso_id = %s where eno = %s"""
         cursor.execute(sql_update_ssoId, (ssoId, eno))
 
         connection.commit()
         count = cursor.rowcount
         print(count, "Record Updated successfully ")
 
-        print("Table After updating record ")
-        sql_select_query = """select * from recource where service_number = %s"""
-        cursor.execute(sql_select_query, (eno,))
-        record = cursor.fetchone()
-        print(record)
+        # print("Table After updating record ")
+        # sql_select_query = """select * from recourse where eno = %s"""
+        # cursor.execute(sql_select_query, (eno,))
+        # record = cursor.fetchone()
+        # print(record)
 
     except (Exception, pg.Error) as error:
         print("Error in update operation", error)
@@ -60,20 +58,20 @@ directory = r'.'
 for i,j,y in os.walk(directory):
     print(i) 
     if "Request" in i:
-        recourceEno = ''
-        recourceCorpId = ''
-        recourceSsoId = ''
+        recourseEno = ''
+        recourseCorpId = ''
+        recourseSsoId = ''
         #   print(os.path.join(directory, filename))
         for filename in os.listdir(i):
-            for t, line in enumerate(open(os.path.join(i, filename))):
+            for t, line in enumerate(open(os.path.join(i, filename), encoding='utf-8')):
                     for match in re.finditer(patternEno, line):
-                        recourceEno = match.group(1)
+                        recourseEno = match.group(1)
                     for match2 in re.finditer(patternCorpId, line):
-                        recourceCorpId = match2.group(1)
+                        recourseCorpId = match2.group(1)
                     for match3 in re.finditer(patternSsoId, line):
-                        recourceSsoId = match3.group(1)
-            print (f'Eno = {recourceEno} CorpId = {recourceCorpId} SsoId = {recourceSsoId}')
-            updateTable(recourceEno, recourceCorpId, recourceSsoId)
+                        recourseSsoId = match3.group(1)
+            print (f'Eno = {recourseEno} CorpId = {recourseCorpId} SsoId = {recourseSsoId}')
+            updateTable(recourseEno, recourseCorpId, recourseSsoId)
 
     else:
         continue
